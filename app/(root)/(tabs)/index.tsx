2f9c5e8e565e3a79eba6629cf25661d4dad1411a
E. Hannah <echannah631@gmail.com>
2025-11-09 17:00:39 -0500

    setting up nativewind for styling

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from "expo-router";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -11,6 +11,9 @@ export default function Index() {
         gap: 20,
       }}
     >
+      <Text className='font-bold text-lg my-10 text-blue-400'>
+        Welcome to ReState
+      </Text>
       <Link href={"/sign-in"}>Sign In</Link>
       <Link href={"/explore"}>Explore</Link>
       <Link href={"/profile"}>Profile</Link>
