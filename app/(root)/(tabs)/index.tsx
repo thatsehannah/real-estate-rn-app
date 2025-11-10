a0134e2b5cea260c7efe563d81da0b565e4912a1
E. Hannah <echannah631@gmail.com>
2025-11-10 11:37:31 -0500

    creating a filter component for filtering by categories

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 import { Card, FeaturedCard } from "@/components/Cards";
+import Filters from "@/components/Filters";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
@@ -59,6 +60,7 @@ export default function Index() {
                 </Text>
               </TouchableOpacity>
             </View>
+            <Filters />
             <View className='flex flex-row gap-5 mt-5'>
               <Card />
               <Card />
