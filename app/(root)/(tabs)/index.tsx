1ee63e6db3f0cad36de1836655b8bde61b5cda32
E. Hannah <echannah631@gmail.com>
2025-11-10 10:48:40 -0500

    creating initial home page view, creating search bar component

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,18 +1,35 @@
-import { Text, View } from "react-native";
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { Image, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        gap: 20,
-      }}
-    >
-      <Text className='font-bold my-10 text-blue-400 font-rubik text-3xl'>
-        Welcome to ReState
-      </Text>
-    </View>
+    <SafeAreaView className='bg-white h-full'>
+      <View className='px-5'>
+        <View className='flex flex-row items-center justify-between mt-5'>
+          <View className='flex flex-row'>
+            <Image
+              source={images.avatar}
+              className='size-12 rounded-full'
+            />
+            <View className='flex flex-col items-start ml-2 justify-center'>
+              <Text className='text-xs font-rubik text-black-100'>
+                Good Morning
+              </Text>
+              <Text className='text-base font-rubik-medium text-black-300'>
+                Elliot
+              </Text>
+            </View>
+          </View>
+          <Image
+            source={icons.bell}
+            className='size-6'
+          />
+        </View>
+      </View>
+      <Search />
+    </SafeAreaView>
   );
 }
