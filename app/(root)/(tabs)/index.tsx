658c8fdec27527e317bca64fa4b0f47043d0ec11
E. Hannah <echannah631@gmail.com>
2025-11-10 11:26:19 -0500

    creating the regular card, adjusting main layout

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -2,7 +2,7 @@ import { Card, FeaturedCard } from "@/components/Cards";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
@@ -30,21 +30,41 @@ export default function Index() {
           />
         </View>
         <Search />
-        <View className='my-5'>
-          <View className='flex flex-row items-center justify-between'>
-            <Text className='text-xl font-rubik-bold text-black-300'>
-              Featured
-            </Text>
-            <TouchableOpacity>
-              <Text className='text-base font-rubik-bold text-primary-300'>
-                See All
+        <ScrollView>
+          <View className='my-5'>
+            <View className='flex flex-row items-center justify-between'>
+              <Text className='text-xl font-rubik-bold text-black-300'>
+                Featured
               </Text>
-            </TouchableOpacity>
+              <TouchableOpacity>
+                <Text className='text-base font-rubik-bold text-primary-300'>
+                  See All
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View className='flex flex-row gap-5 mt-5'>
+              <FeaturedCard />
+              <FeaturedCard />
+            </View>
           </View>
-        </View>
 
-        <FeaturedCard />
-        <Card />
+          <View className='my-5'>
+            <View className='flex flex-row items-center justify-between'>
+              <Text className='text-xl font-rubik-bold text-black-300'>
+                Our Recommendations
+              </Text>
+              <TouchableOpacity>
+                <Text className='text-base font-rubik-bold text-primary-300'>
+                  See All
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View className='flex flex-row gap-5 mt-5'>
+              <Card />
+              <Card />
+            </View>
+          </View>
+        </ScrollView>
       </View>
     </SafeAreaView>
   );
--- components/Cards.tsx
+++ components/Cards.tsx
@@ -52,10 +52,41 @@ export const FeaturedCard = ({ onPress }: CardProps) => {
   );
 };
 
-export const Card = () => {
+export const Card = ({ onPress }: CardProps) => {
   return (
-    <View>
-      <Text>Regular Card</Text>
-    </View>
+    <TouchableOpacity
+      onPress={onPress}
+      className='flex-1 w-full mt-4 px-3 py-4 rounded-lg bg-white shadow-lg shadow-black-100/70 relative'
+    >
+      <View className='flex flex-row items-center absolute px-2 top-5 right-5 bg-white/90 p-1 rounded-full z-50'>
+        <Image
+          source={icons.star}
+          className='size-2.5'
+        />
+        <Text className='text-xs font-rubik-bold text-primary-300 ml-0.5'>
+          4.4
+        </Text>
+      </View>
+      <Image
+        source={images.newYork}
+        className='w-full h-40 rounded-lg'
+      />
+      <View className='flex flex-col mt-2'>
+        <Text className='text-base font-rubik-bold text-black-300'>
+          Cozy Studio
+        </Text>
+        <Text className='text-xs font-rubik text-black-200'>New York, USA</Text>
+        <View className='flex flex-row items-center justify-between mt-2'>
+          <Text className='font-rubik-bold text-primary-300 text-base'>
+            $22452
+          </Text>
+          <Image
+            source={icons.heart}
+            className='size-5 mr-2'
+            tintColor='#191d31'
+          />
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 };
