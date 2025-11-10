5b558939ba1cdfb798603adc36d845015691afde
E. Hannah <echannah631@gmail.com>
2025-11-10 12:27:38 -0500

    using current user avatar and name on home page

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -2,11 +2,13 @@ import { Card, FeaturedCard } from "@/components/Cards";
 import Filters from "@/components/Filters";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
-import images from "@/constants/images";
+import { useGlobalContext } from "@/lib/global-provider";
 import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const { user } = useGlobalContext();
+
   return (
     <SafeAreaView className='bg-white h-full'>
       <FlatList
@@ -22,7 +24,7 @@ export default function Index() {
             <View className='flex flex-row items-center justify-between mt-5'>
               <View className='flex flex-row'>
                 <Image
-                  source={images.avatar}
+                  source={{ uri: user?.avatar }}
                   className='size-12 rounded-full'
                 />
                 <View className='flex flex-col items-start ml-2 justify-center'>
@@ -30,7 +32,7 @@ export default function Index() {
                     Good Morning
                   </Text>
                   <Text className='text-base font-rubik-medium text-black-300'>
-                    Elliot
+                    {user?.name}
                   </Text>
                 </View>
               </View>
